7fa2a60fe2b8ef50c8458b0a2d33fc82166e7e94
avinashay <77995697+avinashay@users.noreply.github.com>
2023-01-29 07:46:47 -0500

    Add files via upload
    live pose estimation code

---
+++ team_solutions/pose_live.py
@@ -0,0 +1,369 @@
+import tensorflow as tf
+import tensorflow_hub as hub
+from tensorflow_docs.vis import embed
+import numpy as np
+import cv2
+import time 
+
+# Import matplotlib libraries
+from matplotlib import pyplot as plt
+from matplotlib.collections import LineCollection
+import matplotlib.patches as patches
+
+# Some modules to display an animation using imageio.
+import imageio
+from IPython.display import HTML, display
+
+#@title Helper functions for visualization
+
+# Dictionary that maps from joint names to keypoint indices.
+KEYPOINT_DICT = {
+    'nose': 0,
+    'left_eye': 1,
+    'right_eye': 2,
+    'left_ear': 3,
+    'right_ear': 4,
+    'left_shoulder': 5,
+    'right_shoulder': 6,
+    'left_elbow': 7,
+    'right_elbow': 8,
+    'left_wrist': 9,
+    'right_wrist': 10,
+    'left_hip': 11,
+    'right_hip': 12,
+    'left_knee': 13,
+    'right_knee': 14,
+    'left_ankle': 15,
+    'right_ankle': 16
+}
+
+# Maps bones to a matplotlib color name.
+KEYPOINT_EDGE_INDS_TO_COLOR = {
+    (0, 1): 'm',
+    (0, 2): 'c',
+    (1, 3): 'm',
+    (2, 4): 'c',
+    (0, 5): 'm',
+    (0, 6): 'c',
+    (5, 7): 'm',
+    (7, 9): 'm',
+    (6, 8): 'c',
+    (8, 10): 'c',
+    (5, 6): 'y',
+    (5, 11): 'm',
+    (6, 12): 'c',
+    (11, 12): 'y',
+    (11, 13): 'm',
+    (13, 15): 'm',
+    (12, 14): 'c',
+    (14, 16): 'c'
+}
+
+
+
+
+def _keypoints_and_edges_for_display(keypoints_with_scores,
+                                     height,
+                                     width,
+                                     keypoint_threshold=0.11):
+  """Returns high confidence keypoints and edges for visualization.
+
+  Args:
+    keypoints_with_scores: A numpy array with shape [1, 1, 17, 3] representing
+      the keypoint coordinates and scores returned from the MoveNet model.
+    height: height of the image in pixels.
+    width: width of the image in pixels.
+    keypoint_threshold: minimum confidence score for a keypoint to be
+      visualized.
+
+  Returns:
+    A (keypoints_xy, edges_xy, edge_colors) containing:
+      * the coordinates of all keypoints of all detected entities;
+      * the coordinates of all skeleton edges of all detected entities;
+      * the colors in which the edges should be plotted.
+  """
+  keypoints_all = []
+  keypoint_edges_all = []
+  edge_colors = []
+  num_instances, _, _, _ = keypoints_with_scores.shape
+  for idx in range(num_instances):
+    kpts_x = keypoints_with_scores[0, idx, :, 1]
+    kpts_y = keypoints_with_scores[0, idx, :, 0]
+    kpts_scores = keypoints_with_scores[0, idx, :, 2]
+    kpts_absolute_xy = np.stack(
+        [width * np.array(kpts_x), height * np.array(kpts_y)], axis=-1)
+    kpts_above_thresh_absolute = kpts_absolute_xy[
+        kpts_scores > keypoint_threshold, :]
+    keypoints_all.append(kpts_above_thresh_absolute)
+
+    for edge_pair, color in KEYPOINT_EDGE_INDS_TO_COLOR.items():
+      if (kpts_scores[edge_pair[0]] > keypoint_threshold and
+          kpts_scores[edge_pair[1]] > keypoint_threshold):
+        x_start = kpts_absolute_xy[edge_pair[0], 0]
+        y_start = kpts_absolute_xy[edge_pair[0], 1]
+        x_end = kpts_absolute_xy[edge_pair[1], 0]
+        y_end = kpts_absolute_xy[edge_pair[1], 1]
+        line_seg = np.array([[x_start, y_start], [x_end, y_end]])
+        keypoint_edges_all.append(line_seg)
+        edge_colors.append(color)
+  if keypoints_all:
+    keypoints_xy = np.concatenate(keypoints_all, axis=0)
+  else:
+    keypoints_xy = np.zeros((0, 17, 2))
+
+  if keypoint_edges_all:
+    edges_xy = np.stack(keypoint_edges_all, axis=0)
+  else:
+    edges_xy = np.zeros((0, 2, 2))
+  return keypoints_xy, edges_xy, edge_colors
+
+
+def draw_prediction_on_image(
+    image, keypoints_with_scores, crop_region=None, close_figure=False,
+    output_image_height=None):
+  """Draws the keypoint predictions on image.
+
+  Args:
+    image: A numpy array with shape [height, width, channel] representing the
+      pixel values of the input image.
+    keypoints_with_scores: A numpy array with shape [1, 1, 17, 3] representing
+      the keypoint coordinates and scores returned from the MoveNet model.
+    crop_region: A dictionary that defines the coordinates of the bounding box
+      of the crop region in normalized coordinates (see the init_crop_region
+      function below for more detail). If provided, this function will also
+      draw the bounding box on the image.
+    output_image_height: An integer indicating the height of the output image.
+      Note that the image aspect ratio will be the same as the input image.
+
+  Returns:
+    A numpy array with shape [out_height, out_width, channel] representing the
+    image overlaid with keypoint predictions.
+  """
+  height, width, channel = image.shape
+  aspect_ratio = float(width) / height
+  fig, ax = plt.subplots(figsize=(12 * aspect_ratio, 12))
+  # To remove the huge white borders
+  fig.tight_layout(pad=0)
+  ax.margins(0)
+  ax.set_yticklabels([])
+  ax.set_xticklabels([])
+  plt.axis('off')
+
+  im = ax.imshow(image)
+  line_segments = LineCollection([], linewidths=(4), linestyle='solid')
+  ax.add_collection(line_segments)
+  # Turn off tick labels
+  scat = ax.scatter([], [], s=60, color='#FF1493', zorder=3)
+
+  (keypoint_locs, keypoint_edges,
+   edge_colors) = _keypoints_and_edges_for_display(
+       keypoints_with_scores, height, width)
+
+  line_segments.set_segments(keypoint_edges)
+  line_segments.set_color(edge_colors)
+  if keypoint_edges.shape[0]:
+    line_segments.set_segments(keypoint_edges)
+    line_segments.set_color(edge_colors)
+  if keypoint_locs.shape[0]:
+    scat.set_offsets(keypoint_locs)
+
+  if crop_region is not None:
+    xmin = max(crop_region['x_min'] * width, 0.0)
+    ymin = max(crop_region['y_min'] * height, 0.0)
+    rec_width = min(crop_region['x_max'], 0.99) * width - xmin
+    rec_height = min(crop_region['y_max'], 0.99) * height - ymin
+    rect = patches.Rectangle(
+        (xmin,ymin),rec_width,rec_height,
+        linewidth=1,edgecolor='b',facecolor='none')
+    ax.add_patch(rect)
+
+  fig.canvas.draw()
+  image_from_plot = np.frombuffer(fig.canvas.tostring_rgb(), dtype=np.uint8)
+  image_from_plot = image_from_plot.reshape(
+      fig.canvas.get_width_height()[::-1] + (3,))
+  plt.close(fig)
+  if output_image_height is not None:
+    output_image_width = int(output_image_height / height * width)
+    image_from_plot = cv2.resize(
+        image_from_plot, dsize=(output_image_width, output_image_height),
+         interpolation=cv2.INTER_CUBIC)
+
+  outkeys = keypoints_with_scores[0][0]
+
+  # print(outkeys.shape)
+
+  # print('key',keypoint_locs.shape)
+
+  # outkeys[:,0:1] = keypoint_locs
+
+  return image_from_plot, outkeys,keypoint_locs
+
+def to_gif(images, fps):
+  """Converts image sequence (4D numpy array) to gif."""
+  imageio.mimsave('./animation.gif', images, fps=fps)
+  return embed.embed_file('./animation.gif')
+
+def progress(value, max=100):
+  return HTML("""
+      <progress
+          value='{value}'
+          max='{max}',
+          style='width: 100%'
+      >
+          {value}
+      </progress>
+  """.format(value=value, max=max))
+
+
+model_name = "movenet_lightning" #@param ["movenet_lightning", "movenet_thunder", "movenet_lightning_f16.tflite", "movenet_thunder_f16.tflite", "movenet_lightning_int8.tflite", "movenet_thunder_int8.tflite"]
+
+# Initialize the TFLite interpreter
+
+
+
+module = hub.load("/home/avinash/Downloads/movenet_singlepose_lightning_4/")
+input_size = 192
+
+
+def movenet(input_image):
+    """Runs detection on an input image.
+
+    Args:
+        input_image: A [1, height, width, 3] tensor represents the input image
+        pixels. Note that the height/width should already be resized and match the
+        expected input resolution of the model before passing into this function.
+
+    Returns:
+        A [1, 1, 17, 3] float numpy array representing the predicted keypoint
+        coordinates and scores.
+    """
+    model = module.signatures['serving_default']
+
+    # SavedModel format expects tensor type of int32.
+    input_image = tf.cast(input_image, dtype=tf.int32)
+    # Run model inference.
+    outputs = model(input_image)
+    # Output is a [1, 1, 17, 3] tensor.
+    keypoints_with_scores = outputs['output_0'].numpy()
+    return keypoints_with_scores
+
+
+if __name__ == "__main__":
+
+    vid = cv2.VideoCapture(0)
+
+    prev_frame_time = 0
+
+    new_frame_time = 0
+  
+    while(True):
+        
+        # Capture the video frame
+        # by frame
+        ret, frame = vid.read()
+    
+        # Display the resulting frame
+        cv2.imshow('frame', frame)
+
+        image = frame
+
+        input_image = tf.expand_dims(image, axis=0)
+        input_image = tf.image.resize_with_pad(input_image, input_size, input_size)
+
+        # Run model inference.
+        keypoints_with_scores = movenet(input_image)
+
+        # Visualize the predictions with image.
+        display_image = tf.expand_dims(image, axis=0)
+        # display_image = tf.cast(tf.image.resize_with_pad(
+        #     display_image, 1280, 1280), dtype=tf.int32)
+        output_overlay, keypoint_xys, keypoint_xy = draw_prediction_on_image(
+            np.squeeze(display_image.numpy(), axis=0), keypoints_with_scores)
+
+        output_overlay = cv2.resize(output_overlay, (640, 480),
+               interpolation = cv2.INTER_LINEAR)
+
+        
+        d ={ 0 : 'chair',
+             1 : 'cobra',
+             2 : 'dog',
+             3 : 'tree',
+             4 : 'warrior'
+        }
+
+
+        n = keypoint_xy[:,0].shape[0]
+
+        if n == 17:
+
+          keypoint_xys[:,0:2] = keypoint_xy
+
+          jj = keypoint_xys.flatten().reshape(1,-1)
+
+          print('jj',jj.shape)
+
+          # tflite_model = 'pose_classifier.tflite'
+          interpreter = tf.lite.Interpreter('/home/avinash/Desktop/movenet/pose_classifier.tflite')
+          interpreter.allocate_tensors()
+          input_details = interpreter.get_input_details()
+          output_details = interpreter.get_output_details()
+
+
+          input_index = interpreter.get_input_details()[0]["index"]
+          interpreter.set_tensor(input_index, jj)
+          #Run the inference
+          interpreter.invoke()
+          output_details = interpreter.get_output_details()
+
+
+          output_data = interpreter.get_tensor(output_details[0]['index'])
+          results = np.squeeze(output_data)
+          top_k = results.argsort()
+
+          output_class_index = top_k[-1]
+
+          new_frame_time = time.time()
+
+          fps = 1/(new_frame_time-prev_frame_time)
+          prev_frame_time = new_frame_time
+
+          fps = int(fps)
+
+          fps = 'fps: '+str(fps)
+
+          outclass = "class: " + d[output_class_index]
+
+          font = cv2.FONT_HERSHEY_SIMPLEX
+    
+          # putting the FPS count on the frame
+          #cv2.putText(output_overlay, fps, (7, 70), font, 1, (150, 255, 0), 3, cv2.LINE_AA)
+
+          cv2.putText(output_overlay, outclass, (7, 70), font, 1, (150, 255, 0), 3, cv2.LINE_AA)
+      
+          # displaying the frame with fps
+
+          cv2.imshow('overlay', output_overlay)
+
+          
+          # the 'q' button is set as the
+          # quitting button you may use any
+          # desired button of your choice
+        
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+          
+        else:
+
+          print("Do something")
+
+
+        
+    # After the loop release the cap object
+    vid.release()
+    # Destroy all the windows
+    cv2.destroyAllWindows()
+        
+
+
+
+    
